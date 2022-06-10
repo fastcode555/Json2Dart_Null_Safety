1ed54af164f08b66bca8cab61e81075548201879
Barry <87644022+fastcode555@users.noreply.github.com>
2022-06-10 18:11:34 +0700

    解决混淆后，无法正常dao跟表名的问题

--- lib/database/base_dao.dart
+++ lib/database/base_dao.dart
@@ -4,47 +4,41 @@ import 'package:sqflite/sqflite.dart';
 /// @date 27/4/22
 /// describe:
 ///@author:Barry
-const String primaryKey = "PRIMARY KEY";
 
 abstract class BaseDao<T extends BaseDbModel> {
-  String? __tableName;
+  String __tableName = '';
+  String _primaryKey = '';
 
-  BaseDao({String? tableName}) {
+  BaseDao(String tableName, String primaryKey) {
     __tableName = tableName;
+    _primaryKey = primaryKey;
   }
 
   Database get _db => BaseDbManager.instance.db;
 
-  String get _table {
-    if (__tableName != null) {
-      return __tableName!;
-    }
-    String daoName = this.runtimeType.toString();
-    __tableName = daoName.replaceRange(daoName.length - 3, daoName.length, "");
-    return __tableName!;
-  }
+  String get table => __tableName;
 
   ///insert into the string
   Future<int> insert(T t, [String? tableName]) {
-    return _db.insertSafe(tableName ?? _table, t);
+    return _db.insertSafe(tableName ?? table, t);
   }
 
   ///insert all the bean
   Future<int> insertAll(List<T> t, [String? tableName]) {
     for (T c in t) {
-      _db.insertSafe(tableName ?? _table, c);
+      _db.insertSafe(tableName ?? table, c);
     }
     return Future.value(0);
   }
 
   ///insert the bean map into the table
   Future<int> insertMap(Map<String, dynamic> t, [String? tableName]) {
-    return _db.insertMap(tableName ?? _table, t);
+    return _db.insertMap(tableName ?? table, t);
   }
 
   ///update the database string
   Future<int> update(T t, [String? tableName]) {
-    return _db.updateSafe(tableName ?? _table, t);
+    return _db.updateSafe(tableName ?? table, t);
   }
 
   ///execute the sql
@@ -56,7 +50,7 @@ abstract class BaseDao<T extends BaseDbModel> {
   Future<void> delete(T t, [String? tableName]) {
     Map<String, dynamic> map = t.primaryKeyAndValue();
     return _db.delete(
-      tableName ?? _table,
+      tableName ?? table,
       where: "${map.keys.first} = ?",
       whereArgs: [map.values.first],
     );
@@ -73,7 +67,7 @@ abstract class BaseDao<T extends BaseDbModel> {
       String? orderBy,
       int? limit,
       int? offset}) async {
-    List<Map<String, Object?>> _lists = await _db.query(tableName ?? _table,
+    List<Map<String, Object?>> _lists = await _db.query(tableName ?? table,
         distinct: distinct,
         columns: columns,
         where: where,
@@ -93,7 +87,7 @@ abstract class BaseDao<T extends BaseDbModel> {
 
   ///查询其中的所有数据
   Future<List<T>?> queryAll([String? tableName]) async {
-    List<Map<String, Object?>> _lists = await _db.query(tableName ?? _table);
+    List<Map<String, Object?>> _lists = await _db.query(tableName ?? table);
     if (_lists.isEmpty) return null;
     List<T> _datas = [];
     for (Map<String, Object?> map in _lists) {
@@ -104,13 +98,13 @@ abstract class BaseDao<T extends BaseDbModel> {
 
   ///according the argument query the data
   Future<T?> queryOne(Object arg, [String? tableName]) async {
-    List<T>? _items = await query(tableName: tableName, where: "$primaryKey = ?", whereArgs: [arg]);
+    List<T>? _items = await query(tableName: tableName, where: "$_primaryKey = ?", whereArgs: [arg]);
     return _items?.first;
   }
 
   ///get the all datas count
   Future<int> queryCount([String? tableName]) async {
-    List<Map<String, dynamic>> _maps = await _db.query(tableName ?? _table, columns: [primaryKey]);
+    List<Map<String, dynamic>> _maps = await _db.query(tableName ?? table, columns: [_primaryKey]);
     return _maps.length;
   }
 
@@ -127,9 +121,9 @@ abstract class BaseDao<T extends BaseDbModel> {
   ///delete the table all the datas
   Future<void> clear([String? tableName]) async {
     try {
-      await _db.execute("delete from ${tableName ?? _table}");
+      await _db.execute("delete from ${tableName ?? table}");
       //reset the auto increase id
-      await _db.execute("update sqlite_sequence SET seq = 0 where name ='${tableName ?? _table}';");
+      await _db.execute("update sqlite_sequence SET seq = 0 where name ='${tableName ?? table}';");
     } catch (e) {
       print(e);
     }
@@ -149,10 +143,8 @@ abstract class BaseDao<T extends BaseDbModel> {
 
   ///delete the table
   Future<void> drop([String? tableName]) async {
-    return _db.execute("DROP TABLE ${tableName ?? _table}");
+    return _db.execute("DROP TABLE ${tableName ?? table}");
   }
 
   T fromJson(Map json);
-
-  String get primaryKey;
 }

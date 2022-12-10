f5cc12faa85fc1d753769b11853b44763c8a2bd8
Barry <87644022+fastcode555@users.noreply.github.com>
2022-12-10 09:44:23 +0700

    feat：对key进行混淆

--- example/lib/res/translation_service.dart
+++ example/lib/res/translation_service.dart
@@ -1,8 +1,14 @@
 import 'package:get/get.dart';
 
-import 'strings.dart';
+import 'string_en.dart';
+import 'string_zh_hans.dart';
+import 'string_zh_hant.dart';
 
 class TranslationService extends Translations {
   @override
-  Map<String, Map<String, String>> get keys => localizedSimpleValues;
+  Map<String, Map<String, String>> get keys => {
+        'zh-Hans_CN': languageZhHans,
+        'en_US': languageEn,
+        'zh-Hant_HK': languageZhHant,
+      };
 }
